05616019205d1582a9ed98b0579f1633af49054b
jakerobinson <jake.robinson@rubrik.com>
2025-04-02 15:32:54 -0400

    remove initialproperties from vm export ps1 example

--- code/Rubrik-Security-Cloud-API/Data-Protection/Data-Center/VMware-vSphere/export.ps1
+++ code/Rubrik-Security-Cloud-API/Data-Protection/Data-Center/VMware-vSphere/export.ps1
@@ -4,11 +4,7 @@ $vsphereClusterId = "00000000-0000-0000-0000-000000000000"
 $vsphereDatastoreId = "00000000-0000-0000-0000-000000000000"
 
 $query = New-RscMutation -GqlMutation vsphereVmExportSnapshotV3
-$query.Var.Input = Get-RscType -Name VsphereVmExportSnapshotV3Input -InitialProperties config.clusterId, `
-    config.hostId, `
-    config.storageLocationId, `
-    config.requiredRecoveryParameters.snapshotId
-
+$query.Var.Input = Get-RscType -Name VsphereVmExportSnapshotV3Input -InitialProperties config
 $query.Var.Input.Id = $vm.id
 $query.Var.Input.config.clusterId = $vsphereClusterId
 $query.Var.Input.config.storageLocationId = $vsphereDatastoreId
